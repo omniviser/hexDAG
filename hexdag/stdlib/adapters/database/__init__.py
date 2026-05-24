c183af903f139ff24c000ec4daf18aeb7bb3db93
jhkwapisz <j.kwapisz@omniviser.ai>
2026-05-24 07:31:28 +0200

    feat: adapters and settings

--- hexdag/stdlib/adapters/database/__init__.py
+++ hexdag/stdlib/adapters/database/__init__.py
@@ -1,6 +1,12 @@
 """Database adapters for HexDAG."""
 
-from hexdag.stdlib.adapters.database.pgvector.pgvector_adapter import PgVectorAdapter
 from hexdag.stdlib.adapters.database.sqlite.sqlite_adapter import SQLiteAdapter
 
-__all__ = ["PgVectorAdapter", "SQLiteAdapter"]
+__all__ = ["SQLiteAdapter"]
+
+try:
+    from hexdag.stdlib.adapters.database.pgvector.pgvector_adapter import PgVectorAdapter
+except ImportError:
+    pass
+else:
+    __all__ += ["PgVectorAdapter"]
